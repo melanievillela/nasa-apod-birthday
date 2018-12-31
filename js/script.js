f34aa4535e0bad32e1fc7f5ada1c82c102a5f9e7
Melanie Villela <melanienvillela@gmail.com>
2018-12-31 11:49:40 -0600

    Update
    Was going to use date input but NASA images do not go back past 1995.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+bday = document.querySelector("#bday");
+/*
+//Default call to NASA to get today's photo based on today's date
 fetch("https://api.nasa.gov/planetary/apod?api_key=vy77lplV6oEhKTgPSzAd8v819F14sxrZXTn028YX")
     .then((response) => {
         return response.json()
@@ -15,15 +18,27 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=vy77lplV6oEhKTgPSzAd8v819F14s
     })   
     .catch((error) => {
         console.log("Error: ", error)
-    })     
+    }) 
+*/    
 
-    window.addEventListener("load", () => {
-        document.querySelector("#birthday-form").addEventListener("submit", (e) => {
-            e.preventDefault();
-            console.log(e);
-            console.log(e.srcElement.value)
-        });
+/*Set min & max value for date selector
+let today =  new Date();
+let year = today.getFullYear();
+let month = today.getMonth()+1;
+let day = today.getDate();
+let todayFormattedDate = `${year}-${month}-${day}`;
+
+bday.setAttribute("max", todayFormattedDate);
+bday.setAttribute("min", `1995-06-16`);
+*/
+
+//Listen for user to click submit to get the date that was inputed.
+window.addEventListener("load", () => {
+    document.querySelector("#birthday-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        console.log(bday.value)
     });
+});
     
 /*
 window.addEventListener("load", function() {
